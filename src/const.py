50092e436e760abb9c9c97fa093d66c3609eff47
PetrCala <cala.p@seznam.cz>
2025-08-07 15:12:56 +0200

    chore: add a link for onsen details

--- src/const.py
+++ src/const.py
@@ -10,6 +10,9 @@ class CONST(NamedTuple):
     ONSEN_URL: str = (
         "https://onsen-hunter.oita-apc.co.jp/dhunter/list_onsen.jsp?d=f8a23e58-f16b-4bb0-8b22-2e6acc46c5d5&o=onsendo&e=onsendo"
     )
+    ONSEN_DETAIL_URL: str = (
+        "https://onsen-hunter.oita-apc.co.jp/dhunter/details_onsen.jsp?d=f8a23e58-f16b-4bb0-8b22-2e6acc46c5d5&o=onsendo&e=onsendo&f=list_onsen.jsp%3Fd%3Df8a23e58-f16b-4bb0-8b22-2e6acc46c5d5%26o%3Donsendo%26e%3Donsendo&t={onsen_id}"
+    )
 
 
 CONST = CONST()
