6ace4a723dca46aa6a9b1e8eb6f405cde3410df6
PetrCala <cala.p@seznam.cz>
2025-10-30 18:43:18 +0900

    chore: remove a redundant key from a mock data generation script

--- src/cli/commands/database/generate_realistic_data.py
+++ src/cli/commands/database/generate_realistic_data.py
@@ -191,7 +191,6 @@ def generate_realistic_data(args: argparse.Namespace) -> None:
                 entry_fee_yen=visit.entry_fee_yen,
                 payment_method=visit.payment_method,
                 weather=visit.weather,
-                time_of_day=visit.time_of_day,
                 temperature_outside_celsius=visit.temperature_outside_celsius,
                 visit_time=visit.visit_time,
                 stay_length_minutes=visit.stay_length_minutes,
